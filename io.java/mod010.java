ae54d5953667b082963db3fe5b5c3d76a611c970
pdfmaster <vbm7654@gmail.com>
2025-08-29 11:57:40 +0530

    Add files via upload

---
+++ io.java/mod010.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double a=22.0/7.0;
+        System.out.printf("%.12f\n",a);
+    }
+}
